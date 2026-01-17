8b4cbe67ae3a2d04247a3f90de55aaa2765bdef4
Rachael <rachung23@gmail.com>
2026-01-17 01:00:46 -0500

    adding projects

--- projects.js
+++ projects.js
@@ -2,20 +2,20 @@ const projects = {
     minesweeperButBetter: {
         title: 'Minesweeper But Better',
         who: 'Solo project',
-        what: 'A final project created in my intro to Python class (15-112) that explores minesweeper with three gamemodes: regular, compeitive, and musical. Complete with clean UI, custom themes, and a store." ',
+        what: 'A final project created in my intro to Python class (15-112) that explores minesweeper with three gamemodes: regular, compeitive, and musical. Complete with clean UI, custom themes, and a store.',
         when: 'Fall 2025 in one week',
         why: 'Minesweeper is a classic game that I enjoy quite a lot, but found aesthetically uninteresting and repetitive, thus leading to many themes and gamemodes.',
         how: 'Using Python, CMU CS 112 Graphics, and built within CS Academy',
         embed: 'https://academy.cs.cmu.edu/sharing/linenDuck437082'
     },
-    project2: {
-        title: 'Video Project',
-        who: 'Team of 3',
-        what: 'A documentary about...',
-        when: 'Spring 2024',
-        why: 'To raise awareness...',
-        how: 'Filmed with Canon...',
-        embed: 'https://www.youtube.com/embed/VIDEO_ID'
+    wishWeekReveal: {
+        title: 'Wish Week Reveal',
+        who: 'Director for video committee of 5 within Student Government at Chaparral High School',
+        what: 'A reveal vide, consisting of a recap of wish weeks and then a way to reveal our wish kid. This project won a 1st place district video award.',
+        when: 'Spring 2025',
+        why: 'Wish Week is a fundraising event for Make-A-Wish Colorado where we raise money for a child with a critical illness. This video sets off the entire fundraising week.',
+        how: 'Coordinating filming times and locations, shooting footage, and editing clips.',
+        embed: "https://www.youtube.com/embed/-YarKRqx_uU"
     }
+}
     // ... all your projects
-};
